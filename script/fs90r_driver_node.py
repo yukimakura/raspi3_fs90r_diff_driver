50cfedbc95c91665cc34eca1c7002f51e9235558
yukimakura <ykazuki0806@gmail.com>
2019-05-29 11:22:14 +0900

    change pwm hard -> soft

--- script/fs90r_driver_node.py
+++ script/fs90r_driver_node.py
@@ -8,31 +8,24 @@ import math
 gpio_pin0 = 19
 gpio_pin1 = 18
 rad = 0.035
-old_x = None
-old_yaw = None
 
 pio = pigpio.pi()
 
 def cmd_vel_CB(data):
-    global old_x
-    global old_yaw
+
     pio.set_mode(gpio_pin0, pigpio.OUTPUT)
     pio.set_mode(gpio_pin1, pigpio.OUTPUT)
     r_speed = data.linear.x + data.angular.z
     l_speed = data.linear.x - data.angular.z
     
-    # print("old_x = "+str(old_x))
-    # if old_x != data.linear.x or old_yaw != data.angular.z :
     r_sec = 1.5 + 0.5*(r_speed/rad/(2*3.141592))
     l_sec = 1.5 - 0.5*(l_speed/rad/(2*3.141592))
     print("r_sec = "+str(r_sec))
     print("l_sec = "+str(l_sec))
     # GPIO18
-    pio.hardware_PWM(gpio_pin0, 50, int(1000000*r_sec/20))
+    pio.set_servo_pulsewidth(gpio_pin0, int(r_sec*1000.0))
     # GPIO19
-    pio.hardware_PWM(gpio_pin1, 50, int(1000000*l_sec/20))
-    old_x = data.linear.x
-    old_yaw = data.angular.y
+    pio.set_servo_pulsewidth(gpio_pin1, int(l_sec*1000.0))
 
 def listener():
 
@@ -43,15 +36,11 @@ def listener():
     # run simultaenously.
     rospy.init_node('listener', anonymous=True)
     rospy.Subscriber("cmd_vel", Twist, cmd_vel_CB)
-    global old_x
-    global old_yaw
-    old_x = 0.0
-    old_yaw = 0.0
     rospy.spin()
     # GPIO18
-    pio.hardware_PWM(gpio_pin0, 50, int(1000000*1.5/20))
+    pio.set_servo_pulsewidth(gpio_pin0, 1500)
     # GPIO19
-    pio.hardware_PWM(gpio_pin1, 50, int(1000000*1.25/20))
+    pio.set_servo_pulsewidth(gpio_pin1, 1500)
     pio.set_mode(gpio_pin0, pigpio.INPUT)
     pio.set_mode(gpio_pin1, pigpio.INPUT)
     pio.stop()
